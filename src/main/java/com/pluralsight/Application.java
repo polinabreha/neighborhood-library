383137381884025bb2c09bf90c708f4941ea8e5b
polinabreha <polinabreha@gmail.com>
2026-04-17 14:12:09 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/Application.java
@@ -0,0 +1,110 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Book[] books = {
+                new Book(1,  "978-0-7432-7356-5", "The Great Gatsby"),
+                new Book(2,  "978-0-06-112008-4", "To Kill a Mockingbird"),
+                new Book(3,  "978-0-14-028329-7", "Seven husbands of Evelyn Hugo" ),
+                new Book(4,  "978-0-7432-7357-2", "Of Mice and Men"),
+                new Book(5,  "978-0-06-093546-9", "The Catcher in the Rye"),
+                new Book(6,  "978-0-7432-7358-9", "Brave New World"),
+                new Book(7,  "978-0-14-243723-0", "The Odyssey"),
+                new Book(8,  "978-0-06-112009-1", "Lord of the Flies"),
+                new Book(9,  "978-0-7432-7359-6", "Animal Farm"),
+                new Book(10, "978-0-14-028330-3", "Fahrenheit 451"),
+                new Book(11, "978-0-06-112010-7", "The Grapes of Wrath"),
+                new Book(12, "978-0-7432-7360-2", "East of Eden"),
+                new Book(13, "978-0-14-243724-7", "Crime and Punishment"),
+                new Book(14, "978-0-06-112011-4", "The Brothers Karamazov"),
+                new Book(15, "978-0-7432-7361-9", "Jane Eyre"),
+                new Book(16, "978-0-14-028331-0", "Pride and Prejudice"),
+                new Book(17, "978-0-06-112012-1", "Wuthering Heights"),
+                new Book(18, "978-0-7432-7362-6", "The Scarlet Letter"),
+                new Book(19, "978-0-14-243725-4", "Moby Dick"),
+                new Book(20, "978-0-06-112013-8", "Don Quixote")
+        };
+
+
+        boolean runningTheProgram = true;
+        boolean found = false;
+
+        while (runningTheProgram) {
+            System.out.println("-------Welcome at the Library!-------");
+            System.out.println("\tWhat would you like to do?");
+            System.out.println("1 - Show Available Books");
+            System.out.println("2 - Show Checked Out Books");
+            System.out.println("3 - Exit");
+            System.out.print("Enter choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+                for (int i = 0; i < books.length; i++) {
+                    if (!books[i].isCheckedOut()) {
+                        System.out.println(books[i].getId() + " Isbn:" +
+                                books[i].getIsbn() + "\n Title: " +
+                                books[i].getTitle());
+                    }
+                }
+                System.out.print("Enter book ID to check out or 0 to go back: ");
+                int selectedId = scanner.nextInt();
+                scanner.nextLine();
+                if (selectedId > books.length || selectedId < 1) {
+                    System.out.println("Please enter a valid book ID:");
+                    continue;
+
+                }
+
+                if(selectedId != 0){
+                    for (int i = 0; i < books.length; i++) {
+                        if (books[i].getId() == selectedId) {
+                            System.out.print("Enter your name: ");
+                            String name = scanner.nextLine();
+
+                            books[i].checkOut(name);
+                            found = true;
+                        }
+
+                    }
+                    if (!found) {
+                        System.out.println("Book not found. Please try again.");
+                    }else{
+                        System.out.println("\t Great choice " );
+                        System.out.println("Book Checked Out Successfully!");
+                    }
+                }
+
+
+
+
+
+
+            }else if (choice == 2) {
+                for (int i = 0; i < books.length; i++) {
+
+                    if (books[i].isCheckedOut()) {
+                        System.out.println(
+                                books[i].getId() + " | " +
+                                        books[i].getIsbn() + " | " +
+                                        books[i].getTitle() + " | Checked out to: " +
+                                        books[i].getCheckedOutTo()
+                        );
+                    }
+                }
+            }else if (choice == 3) {
+                runningTheProgram = false;
+            }
+
+
+        }
+
+
+
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,66 @@
+package com.pluralsight;
+
+public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    public Book(int id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public void checkOut(String name){
+        this.checkedOutTo = name;
+        isCheckedOut = true;
+    }
+    public void checkIn(String name){
+        this.checkedOutTo ="";
+        this.isCheckedOut = false;
+    }
+}
